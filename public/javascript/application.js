fc8aea5c4cc1e2b9de6ff9b9ea4697609cff9587
winterreise <puiwing.tam@gmail.com>
2016-06-01 04:44:34 +0000

    Implemented jquery/handlebars front-end for listing and deleting contacts.

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,52 @@
-$(document).ready(function() {
+/*global $, Handlebars*/
+(function() {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-});
+  var contactTemplate = Handlebars.compile($('#contact-template').html());
+
+  function drawContacts(contacts) {
+    $('#ContactList').empty();
+    if (contacts) {
+      for (var i=0;i<contacts.length;i++) {
+        var row = contactTemplate(contacts[i]); // handlebars makes us an HTML string representing one row in our table
+        $('#ContactList').append(row); // append row html to table
+      }
+    }
+  }
+
+  function refreshContacts() {
+    $.ajax({
+      url: '/contacts',
+      method: 'GET',
+      success: drawContacts
+    });
+  }
+
+  function createContact(name, email) {
+    $.ajax({
+      url: '/contacts',
+      method: 'POST',
+      data: {
+        name: name,
+        email: email
+      },
+      success: refreshContacts
+    });
+  }
+
+  function deleteContact(id) {
+    $.ajax({
+      url: '/contacts/' + id,
+      method: 'DELETE',
+      success: refreshContacts
+    });
+  }
+
+  $(document).ready(function() {
+    // start off by loading all the contacts
+    refreshContacts();
+
+    $(document).on('click','button',function(e){
+      deleteContact($(e.target).attr('data-id'));
+    });
+  });
+})();
